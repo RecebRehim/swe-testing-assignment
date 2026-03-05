766a765a4f6881f941a48d660d03ec2b46624027
RecebRehim <recebrehim05@gmail.com>
2026-03-05 21:44:01 +0200

    implement CLI for calculator

---
+++ quick_calc/cli.py
@@ -0,0 +1,40 @@
+from calculator import Calculator
+
+calc = Calculator()
+
+def run():
+    while True:
+        print("\nQuick Calc")
+        print("1 - Add")
+        print("2 - Subtract")
+        print("3 - Multiply")
+        print("4 - Divide")
+        print("5 - Clear")
+        print("0 - Exit")
+
+        choice = input("Select operation: ")
+
+        if choice == "0":
+            break
+
+        if choice == "5":
+            print("Result:", calc.clear())
+            continue
+
+        a = float(input("Enter first number: "))
+        b = float(input("Enter second number: "))
+
+        if choice == "1":
+            print("Result:", calc.add(a, b))
+        elif choice == "2":
+            print("Result:", calc.subtract(a, b))
+        elif choice == "3":
+            print("Result:", calc.multiply(a, b))
+        elif choice == "4":
+            try:
+                print("Result:", calc.divide(a, b))
+            except ValueError as e:
+                print(e)
+
+if __name__ == "__main__":
+    run()
